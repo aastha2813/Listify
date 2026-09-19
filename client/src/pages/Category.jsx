74026eb4773a4a71e6b9e3b050ee8fbe4aab0d28
Aastha Darji <darjiaastha2804@gmail.com>
2026-09-19 15:35:04 +0530

    Polish Listify UI

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -7,9 +7,7 @@ function Category() {
   // -----------------------------------------
   // BACKEND API URL
   // -----------------------------------------
-  const API_URL = import.meta.env.DEV
-    ? "http://localhost:5000"
-    : import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   // -----------------------------------------
   // CATEGORY NAME → DATABASE ID
